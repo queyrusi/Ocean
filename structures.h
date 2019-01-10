16f9e8855d96125d2c074c6662ff0e0f34d680a0
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2019-01-10 05:47:26 +0100

    Update structures.h
    Structures obsolètes retirée - attention, il y a peut-être des reliquats dans les autres fichiers.

--- structures.h
+++ structures.h
@@ -23,38 +23,3 @@ ClientList *newNode(int sockfd, char* ip) { //creation d'un espace pour un poten
     strncpy(np->name, "NULL", 5);
     return np;
 }
-
-
-/**
- * structure bateau (A enlever ?)
- * ----------------
- */
-typedef struct bateau
-{
-        char nom[MAXNOM];
-        int identifiant;
-        int * pidentifiant;
-        /*beaucoup plus pratique pour l'envoi du buffer initial */
-        char vitesse[2];  
-        /* ajout de la même vitesse en int pour les "vraies" 
-        manipulations */
-        int vitesse_int;
-        char direction[3];
-        char position[10];
-        int x;
-        int y;
-} Bateau;
- 
- 
-/**
- * structure annuaire(A enlever ?)
- * ------------------
- */
-typedef struct annuaire
-{
-	char identifiants_pris[4];
-    struct bateau navire1;
-    struct bateau navire2;
-    struct bateau navire3;
-} Annuaire;
-
